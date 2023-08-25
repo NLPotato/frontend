603e87ee84ffdfe6af9c92b44a98d0dec7b5d483
NLPotato <nlpotato31@gmail.com>
2023-08-25 15:44:54 +0900

    [add] rudimentary

---
+++ src/core/heropy.js
@@ -0,0 +1,13 @@
+///// Component
+
+export class Component {
+  constructor(payload = {}) {
+    const { tagName = 'div' } = payload;
+    this.el = document.createElement(tagName);
+    this.render();
+  }
+
+  render() {
+    // ...
+  }
+}
